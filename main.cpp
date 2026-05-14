4aeaddb31e5769bd683d29370f54a2fdb9869357
arenz <marenz.pelaez@gmail.com>
2026-05-13 19:57:45 -0600

    Prueba de Matriz Dispersa

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main() {
     MatrizDispersa matriz;
 
-    // Insertamos algunos píxeles (simulando el corazón del PDF)
+    // Se insertan algunos píxeles
     matriz.insertarPixel(2, 4, "#e74c3c");
     matriz.insertarPixel(2, 5, "#e74c3c");
     matriz.insertarPixel(3, 3, "#e74c3c");
@@ -16,7 +16,7 @@ int main() {
     matriz.insertarPixel(4, 4, "#e74c3c");
     matriz.insertarPixel(4, 5, "#e74c3c");
 
-    // Verificamos que los colores se guardaron bien
+    // Verifica que los colores se guardaron bien
     cout << "Pixel (2,4): " << matriz.obtenerColor(2, 4) << endl;
     cout << "Pixel (3,3): " << matriz.obtenerColor(3, 3) << endl;
     cout << "Pixel (1,1): " << matriz.obtenerColor(1, 1) << endl; // vacío
